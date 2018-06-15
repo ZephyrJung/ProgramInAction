5d03549a3f5006c44d6409e64f84458880312f30
张煜 <yu.zhang@jd.com>
2018-06-15 19:11:09 +0800

    806 number of lines to write Spring

--- src/main/java/org/b3log/leetcode/P806_NumberOfLinesToWriteString.java
+++ src/main/java/org/b3log/leetcode/P806_NumberOfLinesToWriteString.java
@@ -8,23 +8,27 @@ package org.b3log.leetcode;
 public class P806_NumberOfLinesToWriteString {
     public static void main(String[] args) {
         String S = "abcdefghijklmnopqrstuvwxyz";
+        String S2 = "bbbcccdddaaa";
         int[] widths = new int[]{10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
+        int[] widths2 = new int[]{4, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10};
         int[] result = numberOfLines(widths, S);
+        int[] result2 = numberOfLines(widths2, S2);
         System.out.println(result[0] + "," + result[1]);
+        System.out.println(result2[0] + "," + result2[1]);
     }
 
     public static int[] numberOfLines(int[] widths, String S) {
-        int lines = 0;
-        int remains = 0;
-        int width = 0;
+        int length = 0;
+        int lines = 1;
         for (char c : S.toCharArray()) {
-            width += widths[c - 'a'];
-            if (width >= 100) {
+            length += widths[c - 'a'];
+            if (length > 100 * lines && length % 100 != 0) {
                 lines++;
-                remains = S.length() - S.indexOf(c);
-                width = width > 100 ? widths[c - 'a'] : 0;
+                length = (lines - 1) * 100 + widths[c - 'a'];
             }
         }
-        return new int[]{lines, remains};
+        return new int[]{lines, length % 100};
     }
 }
+
+
