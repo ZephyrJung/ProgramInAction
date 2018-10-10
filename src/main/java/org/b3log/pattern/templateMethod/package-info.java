bc65d9471ff55485cb6ff8477fc9ee02bc7b2c83
张煜 <zephyrjung@126.com>
2018-10-10 23:30:39 +0800

    :construction: 模板方法模式

---
+++ src/main/java/org/b3log/pattern/templateMethod/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @author : yu.zhang
+ * Date : 2018/10/10 下午10:43
+ * Email : yu.zhang@7fresh.com
+ * 如果说策略模式注重的是算法的替换的话，模板方法模式注重的应当是一定流程下，每个流程的可替换性
+ * 即有一套方法的调用顺序或逻辑已经固定，而调用方法的具体实现则可以替换
+ **/
+package org.b3log.pattern.templateMethod;
