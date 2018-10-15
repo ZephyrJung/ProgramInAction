7e9c66e6833b6c3ce23cfcb83996902eb5d113f0
张煜 <zephyrjung@126.com>
2018-10-15 19:03:54 +0800

    :construction: adapter

---
+++ src/main/java/org/b3log/netty/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author : yu.zhang
+ * Date : 2018/10/15 下午2:37
+ * Email : yu.zhang@7fresh.com
+ **/
+package org.b3log.netty;
--- src/main/java/org/b3log/pattern/proxy/package-info.java
+++ src/main/java/org/b3log/pattern/proxy/package-info.java
@@ -4,5 +4,6 @@
  * Email : yu.zhang@7fresh.com
  * 为对象添加一个代理器，代理器和对象实现了相同的接口，但是代理中可以对对象进行一些额外的操作
  * 例如，本来是操作英雄类，添加英雄操作代理类，可以为英雄添加附加的公共技能
+ * @see pattern/decorator/package-info.java
  **/
 package org.b3log.pattern.proxy;
