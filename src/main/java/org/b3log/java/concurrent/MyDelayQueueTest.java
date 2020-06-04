c9dafd17e2cec4ee35f282934ac11232ddf255e1
yu.zhang <2895205695@qq.com>
2020-06-04 14:13:23 +0800

    some change

--- src/main/java/org/b3log/java/concurrent/MyDelayQueueTest.java
+++ src/main/java/org/b3log/java/concurrent/MyDelayQueueTest.java
@@ -1,7 +1,11 @@
 package org.b3log.java.concurrent;
 
+import lombok.Data;
+
 import java.io.IOException;
 import java.util.concurrent.DelayQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author yu.zhang
@@ -42,5 +46,41 @@ public class MyDelayQueueTest {
                 e.printStackTrace();
             }
         }
+    }
+}
+
+@Data
+class DelayedElement implements Delayed {
+
+    private final long delay; //延迟时间
+    private final long expire;  //到期时间
+    private final String msg;   //数据
+
+    public DelayedElement(long delay, String msg) {
+        this.delay = delay;
+        this.msg = msg;
+        expire = System.currentTimeMillis() + delay;    //到期时间 = 当前时间+延迟时间
+    }
+
+    /**
+     * 需要实现的接口，获得延迟时间   用过期时间-当前时间
+     *
+     * @param unit
+     * @return
+     */
+    @Override
+    public long getDelay(TimeUnit unit) {
+        return unit.convert(this.expire - System.currentTimeMillis(), TimeUnit.MILLISECONDS);
+    }
+
+    /**
+     * 用于延迟队列内部比较排序   当前时间的延迟时间 - 比较对象的延迟时间
+     *
+     * @param o
+     * @return
+     */
+    @Override
+    public int compareTo(Delayed o) {
+        return (int) (this.getDelay(TimeUnit.MILLISECONDS) - o.getDelay(TimeUnit.MILLISECONDS));
     }
 }
